ecca8098633b90482799d306f25bf1f846f796a7
Darxxemiyur Bayfaderix <darxxie@bayfaderix.name>
2023-02-22 03:38:28 +0300

    .

--- PersonalRPCStatus/Program.cs
+++ PersonalRPCStatus/Program.cs
@@ -10,19 +10,17 @@ namespace Name.Bayfaderix.Darxxemiyur.PersonalRPCStatus
 		private static DiscordGovernor _governor;
 		private static MyTaskSource _thing;
 		private static CancellationTokenSource _source;
+		private const ulong defID = 985573019084804166;
 
 		private static async Task Main()
 		{
 			_source = new();
 			_thing = new(_source.Token);
-			_governor = new(985573019084804166);
+			_governor = new(defID);
 			var exit = _thing.MyTask.ContinueWith(x => _governor.Shutdown()).Unwrap();
 			AppDomain.CurrentDomain.ProcessExit += OnProcessExit;
-			var bg = new Icon("https://llvm.org/img/DragonMedium.png", IconKind.MediaProxy);
-			var ico = new Icon("https://pngimg.com/uploads/github/github_PNG40.png", IconKind.MediaProxy, "https://github.com/Darxxemiyur/PersonalDiscordRichPresenceStatus");
-			var record1 = new StatusRecord(TimeSpan.FromSeconds(10), "You are getting angry because you haven't reached your goal yet. In the end that'll be why you'll reach it", null, ico, bg);
-			var block = new Block(record1);
-			var doc = new Document(Enumerable.Range(1, 50).Select(x => (IStatusYieldable)new YieldablePointer(block)).Prepend(block));
+
+			var doc = Document.Empty;
 			await _thing.MimicResult(WorkRecords(_governor, doc));
 			await exit;
 		}
